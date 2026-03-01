e67b824c2d6cb420fd5d5a6fb638b2fde60b5668
raaron <rodolfoaaronmercado@gmail.com>
2026-03-01 09:58:40 +0800

    Updated path for profile picture

--- src/MainTitle.tsx
+++ src/MainTitle.tsx
@@ -5,7 +5,7 @@ function MainTitle() {
     <Fragment>
       <header className="header">
         <img
-          src="/public/profilePicture.png"
+          src="/../public/profilePicture.png"
           alt="Portfolio Picture"
           id="profile-picture"
           className="center"
